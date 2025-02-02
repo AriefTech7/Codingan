6a1996f0155cfbeb33da0e3999d6355cbcf1d408
Arief Tech <arifwahyudinasution@gmail.com>
2025-02-02 10:25:32 +0700

    add tuples & set

---
+++ tuple&set.py
@@ -0,0 +1,15 @@
+## tuple & set
+
+# tuple -> tipe data yang tidak bisa diubah 
+
+teman = ("wahyu", "andri", "ronald", "nando")
+print(teman)
+print(teman[2])
+
+
+# set -> tipe data yang tidak memiliki urutan, jadi tidak bisa mengambil elemen sesui indexnya
+
+
+setss = {10,34,64,1,7,1,45,23}
+print(setss)
+print(setss[0])
