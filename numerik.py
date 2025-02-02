af07098d2e6662a8136b65ee6dacb4be3d43d2ea
Arief Tech <arifwahyudinasution@gmail.com>
2025-02-02 10:25:00 +0700

    changes in numerik

---
+++ numerik.py
@@ -0,0 +1,11 @@
+## NUMERIK
+c = 2 + 3.5j 
+# ciri bilangan real adalah int dan float
+# sedangkan bilangan imaginer adalah bilangan kompleks dan ada tambahan j pada angka
+print(c.real)
+print(c.imag)
+
+a1 = 2 + 3j
+a2 = 1 + 4j
+hasil = a1 + a2
+print(hasil)
