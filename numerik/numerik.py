f24780aeffe5e22f3c77bef06e7e1dc33fb32810
Arief Tech <arifwahyudinasution@gmail.com>
2025-04-25 20:26:24 +0700

    add file to new folder

---
+++ numerik/numerik.py
@@ -0,0 +1,34 @@
+## NUMERIK
+c = 2 + 3.5j 
+# ciri bilangan real adalah int dan float
+# sedangkan bilangan imaginer adalah bilangan kompleks dan ada tambahan j pada angka
+print(c.real)
+print(c.imag)
+
+a1 = 2 + 3j
+a2 = 1 + 4j
+hasil = a1 + a2
+print(hasil)
+
+print("bagi dengan /", 9 / 4)
+print(3 + 4)
+print(10 - 8)
+print(2 * 3)
+
+print("======")
+print("bagi dengan //", 9 / 4)# dibulatkan
+print(2 ** 3)# perpangkatan
+
+weight = 53
+height = 175
+
+bmi = weight / (height**2)
+print(bmi)
+
+print(int(4.6))
+print(round(4.6))# membulatkan angka keatas
+
+
+
+
+
